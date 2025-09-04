ee9185a94685f53ef66fc132d93df4ca20b6868f
Evelyn D'Elia <17877131+evelyd@users.noreply.github.com>
2025-09-04 22:37:15 +0000

    enable videos on wandb

--- scripts/rsl_rl/train.py
+++ scripts/rsl_rl/train.py
@@ -49,7 +49,8 @@ import os
 import torch
 from datetime import datetime
 
-from rsl_rl.runners import OnPolicyRunner
+# from rsl_rl.runners import OnPolicyRunner
+from basic_locomotion_dls_isaaclab.runners.on_policy_runner import OnPolicyRunner
 
 from isaaclab.envs import (
     DirectMARLEnv,
@@ -120,7 +121,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     env = RslRlVecEnvWrapper(env)
 
     # create runner from rsl-rl
-    runner = OnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
+    runner = OnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device, video_interval=args_cli.video_interval, video_length=args_cli.video_length)
     # write git state to logs
     runner.add_git_repo_to_log(__file__)
     # save resume path before creating a new log_dir
